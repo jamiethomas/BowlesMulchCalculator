4067f45c66a79b639d57c1196d54e8a56e48a4d5
Jamie Thomas <james.b.thomas@gmail.com>
2015-10-22 21:57:24 -0400

    Add tests

---
+++ test/product.service.test.js
@@ -0,0 +1,22 @@
+// Tests using IIFE Syntax
+(function () {
+
+  describe("Product Service Tests", function() {
+
+    //beforeEach(function() {
+    //person = new Person("John", "Doe")
+    //});
+
+    it("getProducts should have data", function() {
+      // TODO
+    });
+
+    it("getDefaultProduct should equal the first product in the list", function() {
+      // TODO
+    });
+
+
+  });
+
+})();
+//@ sourceURL=pen.js
