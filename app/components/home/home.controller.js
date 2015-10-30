2da4147bda6df21d6416d3520b8928e9688e6742
Jamie Thomas <james.b.thomas@gmail.com>
2015-10-29 22:34:36 -0400

    Improve tests with controller re-design

--- app/components/home/home.controller.js
+++ app/components/home/home.controller.js
@@ -4,26 +4,35 @@
     .controller("HomeController", HomeController);
 
   function HomeController($scope, CalculatorService, ProductService) {
-    $scope.length = 10;
-    $scope.width = 20;
+
+    $scope.valueChanged = valueChanged;
+
+    $scope.length = 0;
+    $scope.width = 0;
     $scope.depth = 3;
+
+    $scope.yards = 0;
+    $scope.buckets = 0;
+    $scope.deliveries = 0;
+    $scope.deliveryFee = 0;
+    $scope.totalCost = 0;
 
     $scope.delivered = false;
 
     $scope.products = ProductService.getProducts();
-
     $scope.selectedProduct = $scope.products[0];
-
-    $scope.valueChanged = valueChanged;
 
     valueChanged();
 
     function valueChanged() {
+
+      var deliveryFee = 25;
+
       $scope.yards = CalculatorService.CubicYardAmount($scope.length, $scope.width, $scope.depth);
       $scope.buckets = CalculatorService.BucketAmount($scope.yards);
       $scope.deliveries = Math.ceil($scope.buckets / $scope.selectedProduct.deliveryLimit);
-      $scope.deliveryFee = $scope.deliveries * 25;
-      $scope.totalCost = $scope.buckets * $scope.selectedProduct.price + ($scope.delivered && $scope.buckets > 0 ? $scope.deliveryFee : 0);
+      $scope.deliveryFee = ($scope.delivered) ? $scope.deliveries * deliveryFee : 0;
+      $scope.totalCost = ($scope.buckets * $scope.selectedProduct.price) + $scope.deliveryFee;
     }
 
   }
--- test/home.controller.tests.js
+++ test/home.controller.tests.js
@@ -10,32 +10,66 @@
 
     beforeEach(angular.mock.inject(function(_$controller_){
       $controller = _$controller_;
+
+      $scope = {};
+      testController = $controller('HomeController', { $scope: $scope });
     }));
 
-    describe('Bucket Amount', function () {
+    it("Check default values", function() {
+      expect($scope.length).toBe(0);
+      expect($scope.width).toBe(0);
+      expect($scope.depth).toBe(3);
+      expect($scope.delivered).toBe(false);
 
-      beforeEach(function() {
-        $scope = {};
-        testController = $controller('HomeController', { $scope: $scope });
-      });
+      expect($scope.yards).toBe(0);
+      expect($scope.buckets).toBe(0);
+      expect($scope.deliveries).toBe(0);
+      expect($scope.deliveryFee).toBe(0);
+      expect($scope.totalCost).toBe(0);
+    });
 
-      it('Buckets should equal double the yard amount', function () {
-        for (i = 0; i <= 100; i++) {
-          expect($scope.BucketAmount(i)).toBe(i * 2);
-        }
-      });
+    it("Should handle negative values", function() {
+      $scope.length = -1;
+      $scope.width = -1;
+      $scope.depth = -1;
+      $scope.valueChanged();
 
-      it('Buckets should handle zero', function() {
-        expect($scope.BucketAmount(0)).toBe(0);
-      });
+      expect($scope.yards).toBe(0);
+      expect($scope.buckets).toBe(0);
+      expect($scope.deliveries).toBe(0);
+      expect($scope.deliveryFee).toBe(0);
+      expect($scope.totalCost).toBe(0);
+    });
 
-      it('Buckets should return zero for negative numbers', function() {
-        expect($scope.BucketAmount(-1)).toBe(0);
-      });
+    it("Should handle calculations correctly for pickup", function() {
+      $scope.length = 10;
+      $scope.width = 10;
+      $scope.depth = 3;
+      $scope.valueChanged();
+
+      expect($scope.yards).toBe(1);
+      expect($scope.buckets).toBe(2);
+      expect($scope.deliveries).toBe(1);
+      expect($scope.deliveryFee).toBe(0);
+      expect($scope.totalCost).toBe(30);
 
     });
 
-  });
+    it("Should handle calculations correctly for delivery", function() {
+      $scope.length = 10;
+      $scope.width = 10;
+      $scope.depth = 3;
+      $scope.delivered = true;
+      $scope.valueChanged();
+
+      expect($scope.yards).toBe(1);
+      expect($scope.buckets).toBe(2);
+      expect($scope.deliveries).toBe(1);
+      expect($scope.deliveryFee).toBe(25);
+      expect($scope.totalCost).toBe(55);
+
+    });
+});
 
 })();
 //@ sourceURL=pen.js
