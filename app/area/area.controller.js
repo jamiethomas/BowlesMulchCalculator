d3a8124940278a5aa16438279e14248164cde033
Jamie Thomas <james.b.thomas@gmail.com>
2015-11-26 08:21:16 -0500

    Update code using aliases for controllers. Correct tests

--- app/area/area.controller.js
+++ app/area/area.controller.js
@@ -7,28 +7,28 @@
 
     var vm = this;
 
-    this.calculateOrder = calculateOrder;
+    vm.calculateOrder = calculateOrder;
 
     $scope.vmMain.title = "Area Calculator";
     $scope.vmMain.description = "Use this handy calculator to determine how much product you need for your coverage area.";
 
-    this.length = 0;
-    this.width = 0;
-    this.depth = 3;
-    this.delivered = false;
+    vm.length = 0;
+    vm.width = 0;
+    vm.depth = 3;
+    vm.delivered = false;
 
-    this.products = ProductService.getProducts();
-    this.selectedProduct = this.products[0];
+    vm.products = ProductService.getProducts();
+    vm.selectedProduct = vm.products[0];
 
-    this.calculateOrder();
+    vm.calculateOrder();
 
     function calculateOrder() {
-      this.order = CalculatorService.CalculateOrderByArea(
-        this.length,
-        this.width,
-        this.depth,
-        this.selectedProduct,
-        this.delivered,
+      vm.order = CalculatorService.CalculateOrderByArea(
+        vm.length,
+        vm.width,
+        vm.depth,
+        vm.selectedProduct,
+        vm.delivered,
         CONFIG.DELIVERY_FEE
       );
     }
--- app/area/area.controller.spec.js
+++ app/area/area.controller.spec.js
@@ -12,72 +12,72 @@
       $controller = _$controller_;
 
       $scope = {};
-      $scope.$parent = {};
-      testController = $controller('AreaController', { $scope: $scope });
+      $scope.vmMain = {};
+      vm = $controller('AreaController', { $scope: $scope });
     }));
 
     it("Check default values", function() {
-      expect($scope.$parent.title).toBe("Area Calculator");
-      expect($scope.$parent.description).toBe("Use this handy calculator to determine how much product you need for your coverage area.")
+      expect($scope.vmMain.title).toBe("Area Calculator");
+      expect($scope.vmMain.description).toBe("Use this handy calculator to determine how much product you need for your coverage area.")
 
       // Form values
-      expect($scope.length).toBe(0);
-      expect($scope.width).toBe(0);
-      expect($scope.depth).toBe(3);
-      expect($scope.delivered).toBe(false);
+      expect(vm.length).toBe(0);
+      expect(vm.width).toBe(0);
+      expect(vm.depth).toBe(3);
+      expect(vm.delivered).toBe(false);
 
       // Check Order
-      expect($scope.order).not.toBe(null);
-      expect($scope.order.yards).toBe(0);
-      expect($scope.order.buckets).toBe(0);
-      expect($scope.order.deliveries).toBe(0);
-      expect($scope.order.totalCost).toBe(0);
-      expect($scope.order.deliveryCost).toBe(0);
+      expect(vm.order).not.toBe(null);
+      expect(vm.order.yards).toBe(0);
+      expect(vm.order.buckets).toBe(0);
+      expect(vm.order.deliveries).toBe(0);
+      expect(vm.order.totalCost).toBe(0);
+      expect(vm.order.deliveryCost).toBe(0);
 
     });
 
     it("Should handle negative values", function() {
-      $scope.length = -1;
-      $scope.width = -1;
-      $scope.depth = -1;
-      $scope.calculateOrder();
+      vm.length = -1;
+      vm.width = -1;
+      vm.depth = -1;
+      vm.calculateOrder();
 
       // Caluclated values
-      expect($scope.order.yards).toBe(0);
-      expect($scope.order.buckets).toBe(0);
-      expect($scope.order.deliveries).toBe(0);
-      expect($scope.order.totalCost).toBe(0);
-      expect($scope.order.deliveryCost).toBe(0);
+      expect(vm.order.yards).toBe(0);
+      expect(vm.order.buckets).toBe(0);
+      expect(vm.order.deliveries).toBe(0);
+      expect(vm.order.totalCost).toBe(0);
+      expect(vm.order.deliveryCost).toBe(0);
     });
 
     it("Should handle calculations correctly for pickup", function() {
-      $scope.length = 10;
-      $scope.width = 10;
-      $scope.depth = 3;
-      $scope.calculateOrder();
+      vm.length = 10;
+      vm.width = 10;
+      vm.depth = 3;
+      vm.calculateOrder();
 
       // Calculated values
-      expect($scope.order.yards).toBe(1);
-      expect($scope.order.buckets).toBe(2);
-      expect($scope.order.deliveries).toBe(1);
-      expect($scope.order.deliveryCost).toBe(25); // Delivery fee is calculated but not applied
-      expect($scope.order.totalCost).toBe(30);
+      expect(vm.order.yards).toBe(1);
+      expect(vm.order.buckets).toBe(2);
+      expect(vm.order.deliveries).toBe(1);
+      expect(vm.order.deliveryCost).toBe(25); // Delivery fee is calculated but not applied
+      expect(vm.order.totalCost).toBe(30);
 
     });
 
     it("Should handle calculations correctly for delivery", function() {
-      $scope.length = 10;
-      $scope.width = 10;
-      $scope.depth = 3;
-      $scope.delivered = true;
-      $scope.calculateOrder();
+      vm.length = 10;
+      vm.width = 10;
+      vm.depth = 3;
+      vm.delivered = true;
+      vm.calculateOrder();
 
       // Calculated values
-      expect($scope.order.yards).toBe(1);
-      expect($scope.order.buckets).toBe(2);
-      expect($scope.order.deliveries).toBe(1);
-      expect($scope.order.deliveryCost).toBe(25);
-      expect($scope.order.totalCost).toBe(55);
+      expect(vm.order.yards).toBe(1);
+      expect(vm.order.buckets).toBe(2);
+      expect(vm.order.deliveries).toBe(1);
+      expect(vm.order.deliveryCost).toBe(25);
+      expect(vm.order.totalCost).toBe(55);
 
     });
 });
