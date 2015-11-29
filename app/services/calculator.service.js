aeff079ad8620665048f2742697d26f6d6dc325c
Jamie Thomas <james.b.thomas@gmail.com>
2015-11-28 22:15:02 -0500

    Add roundHalf function and implement. Rename functions to javascript best practices

--- app/services/calculator.service.js
+++ app/services/calculator.service.js
@@ -8,63 +8,22 @@
     function CalculatorService() {
 
       var factory = {
-        CubicYardAmount : CubicYardAmount,
-        BucketAmount : BucketAmount,
-        BagsToYards : BagsToYards,
-        BucketsToYards : BucketsToYards,
+        cubicYardAmount : cubicYardAmount,
+        bucketAmount : bucketAmount,
+        bagsToYards : bagsToYards,
+        bucketsToYards : bucketsToYards,
         CalculateOrderByArea : CalculateOrderByArea,
         CalculateOrderByBags : CalculateOrderByBags,
-        CalculateOrderByBuckets : CalculateOrderByBuckets
+        CalculateOrderByBuckets : CalculateOrderByBuckets,
+        roundHalf : roundHalf
       };
-
-      function CubicYardAmount(length, width, depth) {
-
-        var yards = 0;
-
-        if (length > 0 && width > 0 && depth > 0) {
-          yards = Math.round(length * width * (depth / 12) / 27);
-        }
-
-        return yards;
-      }
-
-      function BucketAmount(yards) {
-
-        var buckets = 0;
-
-        if (yards > 0) {
-          buckets = yards * 2
-        }
-
-        return buckets;
-      }
-
-      function BagsToYards(size, count) {
-        var yards = 0;
-
-        if (size > 0 && count > 0) {
-          yards = Math.round(((size * count) / 27));
-        }
-
-        return yards;
-      }
-
-      function BucketsToYards(buckets) {
-        var yards = 0;
-
-        if (buckets > 0 && buckets > 0) {
-          yards = buckets / 2;
-        }
-
-        return yards;
-      }
 
       function CalculateOrderByArea(length, width, depth, selectedProduct, delivered, deliveryFee) {
 
          var order = {};
 
-         order.yards = CubicYardAmount(length, width, depth);
-         order.buckets = BucketAmount(order.yards);
+         order.yards = cubicYardAmount(length, width, depth);
+         order.buckets = bucketAmount(order.yards);
          order.deliveries = Math.ceil(order.buckets / selectedProduct.deliveryLimit);
          order.deliveryCost = (order.buckets > 0) ? order.deliveries * deliveryFee : 0;
          order.totalCost = (order.buckets * selectedProduct.price) + ((delivered) ?  order.deliveryCost : 0);
@@ -78,8 +37,8 @@
 
         var order = {};
 
-        order.yards = BagsToYards(size, count);
-        order.buckets = BucketAmount(order.yards);
+        order.yards = bagsToYards(size, count);
+        order.buckets = bucketAmount(order.yards);
         order.deliveries = Math.ceil(order.buckets / selectedProduct.deliveryLimit);
         order.deliveryCost = (order.buckets > 0) ? order.deliveries * deliveryFee : 0;
         order.totalCost = (order.buckets * selectedProduct.price) + ((delivered) ?  order.deliveryCost : 0);
@@ -94,8 +53,8 @@
 
         var order = {};
 
-        order.yards = BucketsToYards(buckets);
-        order.buckets = BucketAmount(order.yards);
+        order.yards = bucketsToYards(buckets);
+        order.buckets = bucketAmount(order.yards);
         order.deliveries = Math.ceil(order.buckets / selectedProduct.deliveryLimit);
         order.deliveryCost = (order.buckets > 0) ? order.deliveries * deliveryFee : 0;
         order.totalCost = (order.buckets * selectedProduct.price) + ((delivered) ?  order.deliveryCost : 0);
@@ -103,6 +62,60 @@
         order.selectedProduct = selectedProduct;
 
         return order;
+      }
+
+      // Private methods
+
+      function cubicYardAmount(length, width, depth) {
+
+        var yards = 0;
+
+        if (length > 0 && width > 0 && depth > 0) {
+          yards = roundHalf(length * width * (depth / 12) / 27);
+        }
+
+        return yards;
+      }
+
+      function bucketAmount(yards) {
+
+        var buckets = 0;
+
+        if (yards > 0) {
+          buckets = yards * 2
+        }
+
+        return buckets;
+      }
+
+      function bagsToYards(size, count) {
+        var yards = 0;
+
+        if (size > 0 && count > 0) {
+          yards = roundHalf(((size * count) / 27));
+        }
+
+        return yards;
+      }
+
+      function bucketsToYards(buckets) {
+        var yards = 0;
+
+        if (buckets > 0 && buckets > 0) {
+          yards = buckets / 2;
+        }
+
+        return yards;
+      }
+
+      function roundHalf(num) {
+        var result = 0;
+
+        if (num > 0) {
+          result = Math.round(num * 2) / 2;
+        }
+
+        return result;
       }
 
      return factory;
--- app/services/calculator.service.spec.js
+++ app/services/calculator.service.spec.js
@@ -337,55 +337,55 @@
 
     });
 
-    describe("BucketAmount Tests", function() {
+    describe("bucketAmount Tests", function() {
 
-      it('BucketAmount should equal double the yard amount', function () {
+      it('bucketAmount should equal double the yard amount', function () {
         for (i = 0; i <= 100; i++) {
-          expect(testService.BucketAmount(i)).toBe(i * 2);
+          expect(testService.bucketAmount(i)).toBe(i * 2);
         }
       });
 
-      it('BucketAmount should handle zero', function() {
-        expect(testService.BucketAmount(0)).toBe(0);
+      it('bucketAmount should handle zero', function() {
+        expect(testService.bucketAmount(0)).toBe(0);
       });
 
-      it('BucketAmount should return zero for negative numbers', function() {
-        expect(testService.BucketAmount(-1)).toBe(0);
+      it('bucketAmount should return zero for negative numbers', function() {
+        expect(testService.bucketAmount(-1)).toBe(0);
       });
 
     });
 
-    describe("CubicYardAmount Tests", function() {
+    describe("cubicYardAmount Tests", function() {
 
-      it("CubicYardAmount should handle zero", function() {
-        expect(testService.CubicYardAmount(0, 0, 0)).toBe(0);
+      it("cubicYardAmount should handle zero", function() {
+        expect(testService.cubicYardAmount(0, 0, 0)).toBe(0);
       });
 
-      it("CubicYardAmount should handle negative numbers", function() {
-        expect(testService.CubicYardAmount(-1, -1, -1)).toBe(0);
-        expect(testService.CubicYardAmount(-3, -9, -12)).toBe(0);
+      it("cubicYardAmount should handle negative numbers", function() {
+        expect(testService.cubicYardAmount(-1, -1, -1)).toBe(0);
+        expect(testService.cubicYardAmount(-3, -9, -12)).toBe(0);
       });
 
-      it("CubicYardAmount should do the math correctly", function() {
-        expect(testService.CubicYardAmount(3, 9, 12)).toBe(1);
+      it("cubicYardAmount should do the math correctly", function() {
+        expect(testService.cubicYardAmount(3, 9, 12)).toBe(1);
       });
 
-      it("CubicYardAmount should should round up", function() {
-        expect(testService.CubicYardAmount(2, 2, 3)).toBe(0);
+      it("cubicYardAmount should should round up to nearest .5", function() {
+        expect(testService.cubicYardAmount(2, 2, 3)).toBe(0);
         expect(yardsCheck(2, 2, 3)).toBeLessThan(0.5);
 
-        expect(testService.CubicYardAmount(3, 3, 12)).toBe(0);
+        expect(testService.cubicYardAmount(3, 3, 12)).toBe(.5);
         expect(yardsCheck(3, 3, 12)).toBeLessThan(0.5);
 
-        expect(testService.CubicYardAmount(3, 5, 12)).toBe(1);
+        expect(testService.cubicYardAmount(3, 5, 12)).toBe(.5);
         expect(yardsCheck(3, 5, 12)).toBeGreaterThan(0.5);
 
-        expect(testService.CubicYardAmount(6, 7, 12)).toBe(2);
+        expect(testService.cubicYardAmount(6, 7, 12)).toBe(1.5);
         expect(yardsCheck(6, 7, 12)).toBeGreaterThan(1.5);
 
       });
 
-      // Check Function for CubicYardAmount
+      // Check Function for cubicYardAmount
 
       function yardsCheck(length, width, depth) {
         return ((length * width * (depth / 12) ) / 27 );
@@ -393,38 +393,38 @@
 
     });
 
-    describe("BagsToYards Tests", function() {
+    describe("bagsToYards Tests", function() {
 
-      it("BagsToYards should be defined", function () {
-        expect(testService.BagsToYards).toBeDefined();
+      it("bagsToYards should be defined", function () {
+        expect(testService.bagsToYards).toBeDefined();
       });
 
-      it("BagsToYards should handle zero", function () {
-        expect(testService.BagsToYards(0, 0)).toBe(0);
+      it("bagsToYards should handle zero", function () {
+        expect(testService.bagsToYards(0, 0)).toBe(0);
       });
 
-      it("BagsToYards should handle negative numbers", function () {
-        expect(testService.BagsToYards(-1, -1)).toBe(0);
+      it("bagsToYards should handle negative numbers", function () {
+        expect(testService.bagsToYards(-1, -1)).toBe(0);
       });
 
       it("18 1.5 Cubic Yard Bags should equal 1 yard", function() {
-        expect(testService.BagsToYards(1.5, 18)).toBe(1);
+        expect(testService.bagsToYards(1.5, 18)).toBe(1);
       });
 
       it("13.5 2 Cubic Yard Bags should equal 1 yard", function() {
-        expect(testService.BagsToYards(2, 13.5)).toBe(1);
+        expect(testService.bagsToYards(2, 13.5)).toBe(1);
       });
 
       it("9 3 Cubic Yard Bags should equal 1 yard", function() {
-        expect(testService.BagsToYards(3, 9)).toBe(1);
+        expect(testService.bagsToYards(3, 9)).toBe(1);
       });
 
-      it("BagsToYards should round up", function() {
-        expect(testService.BagsToYards(3, 14)).toBe(2);
+      it("bagsToYards should round up", function() {
+        expect(testService.bagsToYards(3, 14)).toBe(1.5);
         expect(bagCheck(3, 14)).toBeGreaterThan(1.5);
       });
 
-      // Check Function for BagsToYards
+      // Check Function for bagsToYards
 
       function bagCheck(size, count) {
         return (size * count) / 27
@@ -432,35 +432,52 @@
 
     });
 
-    describe("BucketsToYards Tests", function() {
+    describe("bucketsToYards Tests", function() {
 
-      it("BucketsToYards should be defined", function () {
-        expect(testService.BucketsToYards).toBeDefined();
+      it("bucketsToYards should be defined", function () {
+        expect(testService.bucketsToYards).toBeDefined();
       });
 
-      it("BucketsToYards should handle zero", function () {
-        expect(testService.BucketsToYards(0)).toBe(0);
+      it("bucketsToYards should handle zero", function () {
+        expect(testService.bucketsToYards(0)).toBe(0);
       });
 
-      it("BucketsToYards should handle negative numbers", function () {
-        expect(testService.BucketsToYards(-1)).toBe(0);
+      it("bucketsToYards should handle negative numbers", function () {
+        expect(testService.bucketsToYards(-1)).toBe(0);
       });
 
-      it("BucketsToYards should NOT round up", function() {
-        expect(testService.BucketsToYards(3)).toBe(1.5);
+      it("bucketsToYards should NOT round up", function() {
+        expect(testService.bucketsToYards(3)).toBe(1.5);
         expect(bucketCheck(3)).toEqual(1.5);
 
         for (i = 1; i < 100; i++) {
-          expect(testService.BucketsToYards(i)).toBe(i / 2);
+          expect(testService.bucketsToYards(i)).toBe(i / 2);
           expect(bucketCheck(i)).toEqual(i / 2);
         }
       });
 
-      // Check Function for BucketsToYards
+      // Check Function for bucketsToYards
 
       function bucketCheck(buckets) {
         return (buckets) / 2
       }
+
+    });
+
+    describe("roundHalf Tests", function() {
+
+      it("roundHalf should be defined", function () {
+        expect(testService.roundHalf).toBeDefined();
+      });
+
+      it("roundHalf should handle zero", function () {
+        expect(testService.roundHalf(0)).toBe(0);
+      });
+
+      it("roundHalf should handle negative numbers", function () {
+        expect(testService.roundHalf(-1)).toBe(0);
+      });
+
     });
 
   });
