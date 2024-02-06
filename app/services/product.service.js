1e96ff10318097da69b8f76287f41df14cbf0f12
Jamie Thomas <james.b.thomas@gmail.com>
2024-02-06 17:45:16 -0500

    Change price from $15 to $17

--- app/services/product.service.js
+++ app/services/product.service.js
@@ -1,31 +1,40 @@
 (function () {
     'use strict';
 
-    angular
-      .module('app')
-      .factory("ProductService", ProductService);
+    angular.module('app').factory('ProductService', ProductService);
 
     function ProductService() {
+        var factory = {
+            getProducts: getProducts,
+            getDefaultProduct: getDefaultProduct,
+        };
 
-      var factory = {
-        getProducts : getProducts,
-        getDefaultProduct : getDefaultProduct
-      };
+        function getProducts() {
+            return [
+                {
+                    name: 'Natural Shredded Hardwood',
+                    price: 17,
+                    deliveryLimit: 20,
+                },
+                {
+                    name: 'Brown Shredded Hardwood',
+                    price: 17,
+                    deliveryLimit: 20,
+                },
+                {
+                    name: 'Black Shredded Hardwood',
+                    price: 17,
+                    deliveryLimit: 20,
+                },
+            ];
 
-      function getProducts() {
-        return [
-          { name: "Natural Shredded Hardwood", price: 15, deliveryLimit: 20 },
-          { name: "Brown Shredded Hardwood", price: 15, deliveryLimit: 20 },
-          { name: "Black Shredded Hardwood", price: 15, deliveryLimit: 20 },
-        ];
+            //{ name: "Manure", price: 15, deliveryLimit: 10 },
+        }
 
-        //{ name: "Manure", price: 15, deliveryLimit: 10 },
-      }
+        function getDefaultProduct() {
+            return getProducts()[0];
+        }
 
-      function getDefaultProduct() {
-        return getProducts()[0];
-      }
-
-     return factory;
-   }
+        return factory;
+    }
 })();
